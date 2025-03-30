8ddba076afdace5811a75b9c4332ec12acc8a48e
HastingsJack <joha0005@stud.kea.dk>
2025-03-30 21:07:42 +0200

    Fixed some things to hopefully fix build error

--- src/main/java/org/example/oenskelisten/Interface/IWishRepository.java
+++ src/main/java/org/example/oenskelisten/Interface/IWishRepository.java
@@ -1,7 +1,7 @@
 package org.example.oenskelisten.Interface;
 
-public interface IWishRepository<T> {
-    T getByName(String name);
-    void add(T item);
+public interface IWishRepository<Wish> extends IRepository<Wish> {
+    Wish getByName(String name);
+    void add(Wish item);
     void deleteById(int id);
 }
--- src/main/java/org/example/oenskelisten/Repository/WishListRepository.java
+++ src/main/java/org/example/oenskelisten/Repository/WishListRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class WishListRepository extends BaseRepository implements IRepository<Wish>, IWishRepository<Wish> {
+public class WishListRepository extends BaseRepository implements IWishRepository<Wish> {
 
     public WishListRepository(JdbcTemplate jdbcTemplate) {
         super(jdbcTemplate);
